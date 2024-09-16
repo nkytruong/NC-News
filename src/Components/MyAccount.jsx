6141a31450e567c128652a9cd6832b4c71b8d9ad
Nikki Truong <nikki@Nikkis-MacBook-Pro.local>
2024-09-16 22:05:09 +0100

    Adds margin for My Account page, adds alert for logging out, adds redirect to home page after logged out

--- src/Components/MyAccount.jsx
+++ src/Components/MyAccount.jsx
@@ -1,23 +1,43 @@
 import * as React from "react";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 import { UserContext } from "./UserContext";
 import { useContext } from "react";
+import Alert from "@mui/material/Alert";
+import CheckIcon from "@mui/icons-material/Check";
+import { useNavigate } from "react-router-dom";
 
-function MyAccount({isLoggedIn, setIsLoggedIn}) {
-    const { user, setUser } = useContext(UserContext);
 
-    function handleSignOutClick(e) {
-        setUser("")
-        setIsLoggedIn(false)
-        }
 
-    return (
-    <div>
-        <h1>My Account</h1>
-        <h2>Welcome {user}</h2>
-        <Button variant="outlined" onClick={handleSignOutClick}>Sign Out</Button>
+function MyAccount({ isLoggedIn, setIsLoggedIn }) {
+  const { user, setUser } = useContext(UserContext);
+  const navigate = useNavigate();
+
+
+  function handleSignOutClick(e) {
+    setUser("");
+    setIsLoggedIn(false);
+    setTimeout(() => {
+      navigate(`/`);
+    }, 2000);
+  }
+
+  return (
+    <div className="page-container">
+      <h1>My Account</h1>
+      {user ? (
+        <div>
+          <h2>Welcome {user}</h2>
+          <Button variant="outlined" onClick={handleSignOutClick}>
+            Sign Out
+          </Button>
+        </div>
+      ) : (
+        <Alert icon={<CheckIcon fontSize="inherit" />} severity="success">
+          Logging out...
+        </Alert>
+      )}
     </div>
-    )
+  );
 }
 
-export default MyAccount
+export default MyAccount;
